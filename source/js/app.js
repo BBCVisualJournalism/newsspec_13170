3e44b142cbe23e490acdc35901ef261415119d4e
Punit Shah <punit.shah@bbc.co.uk>
2015-12-03 12:26:40 +0000

    removed label, using placeholder instead
    implemented with value that disappears on focus so it works in IE9-

--- source/js/app.js
+++ source/js/app.js
@@ -2,7 +2,6 @@ define(['bootstrap', 'autocompleteMediator', 'http://www.live.bbc.co.uk/indeptht
 
     var $searchForm = news.$('.mpSearch_form');
     var $searchInput = news.$('#mpSearch_form_input');
-    var $searchSubmit = news.$('.mpSearch_form_submit');
 
     var $result = news.$('.mpSearch_result');
     var $resultVote = news.$('.mpSearch_result_vote');
@@ -30,29 +29,52 @@ define(['bootstrap', 'autocompleteMediator', 'http://www.live.bbc.co.uk/indeptht
 
     var autocomplete = new AutocompleteMediator($searchInput, hideResult, autocompleteData);
 
-    $searchForm.on('submit', function () {
-        news.pubsub.emit('user-submitted-mp', [getUserMp()]);
+    $searchInput.on('focus', function () {
+        var $this = $(this);
+        if ($this.val() === $this.attr('placeholder')) {
+            $this.val('');
+            $this.removeClass('mpSearch_form_input-placeholder');
+        }
+    }).on('blur', function () {
+        var $this = $(this);
+        if ($this.val() === '' || $this.val() === $this.attr('placeholder')) {
+            $this.addClass('mpSearch_form_input-placeholder');
+            $this.val($this.attr('placeholder'));
+        }
+    }).trigger('blur');
 
-        news.pubsub.emit('istats', ['mp-submitted', 'newsspec-interaction', true]);
-        var istats_mpname = 'mp-submitted_' + getUserMp().data.mp_name.replace(/\s+/g, '-').toLowerCase();
-        news.pubsub.emit('istats', [istats_mpname, 'newsspec-interaction', true]);
+    $searchForm.on('submit', function () {
+        if ($searchInput.val() === $searchInput.attr('placeholder')) {
+            $searchInput.val('');
+        }
+
+        var userMp = getUserMp();
+        if (userMp) {
+            news.pubsub.emit('user-submitted-mp', [userMp]);
+
+            news.pubsub.emit('istats', ['mp-submitted', 'newsspec-interaction', true]);
+            var istats_mpname = 'mp-submitted_' + userMp.data.mp_name.replace(/\s+/g, '-').toLowerCase();
+            news.pubsub.emit('istats', [istats_mpname, 'newsspec-interaction', true]);
+        }
         return false;
     });
 
     news.pubsub.on('user-submitted-mp', function (mp) {
-        showResult();
-        $resultVote.text(mp.data.vote_outcome);
+        if (mp) {
+            showResult();
+            $resultVote.text(mp.data.vote_outcome);
 
-        var resultHtmlString = '<p>' + mp.data.mp_name + ' (' + mp.data.party + ', ' + mp.data.constituency_name + ')';
-        if (mp.data.vote_outcome.toLowerCase() === 'abstained') {
-            resultHtmlString += ' abstained from voting.';
-        } else if (mp.data.vote_outcome.toLowerCase() === 'did not vote') {
-            resultHtmlString += ' did not vote.';
-        } else {
-            resultHtmlString += ' voted ' + mp.data.vote_outcome.toLowerCase() + ' the motion.';
+            var resultHtmlString = '<p>' + mp.data.mp_name + ' (' + mp.data.party + ', ' + mp.data.constituency_name + ')';
+            if (mp.data.vote_outcome.toLowerCase() === 'abstained') {
+                resultHtmlString += ' abstained from voting.';
+            } else if (mp.data.vote_outcome.toLowerCase() === 'did not vote') {
+                resultHtmlString += ' did not vote.';
+            } else {
+                resultHtmlString += ' voted ' + mp.data.vote_outcome.toLowerCase() + ' the motion.';
+            }
+            resultHtmlString += '</p>';
+            $resultText.html(resultHtmlString);
         }
-        resultHtmlString += '</p>';
-        $resultText.html(resultHtmlString);
     });
 
     news.pubsub.emit('pageLoaded');
