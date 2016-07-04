af9dbfa338939f5572035c864c92a45a64b912c4
Punit Shah <punit.shah@bbc.co.uk>
2016-07-04 17:01:49 +0100

    updated dataset url - live

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,4 @@
-define(['bootstrap', 'autocompleteMediator', 'http://www.stage.bbc.co.uk/indepthtoolkit/data-sets/how_did_my_mp_vote/jsonp'], function (news, AutocompleteMediator, autocompleteData) {
+define(['bootstrap', 'autocompleteMediator', 'http://www.bbc.co.uk/indepthtoolkit/data-sets/how_did_my_mp_vote/jsonp'], function (news, AutocompleteMediator, autocompleteData) {
 
     var $searchForm = news.$('.mpSearch_form');
     var $searchInput = news.$('#mpSearch_form_input');
